a9efb9796dc2b81c6e7a50e500c1932b35779d3b
Natalija <85802391+natalijaandric@users.noreply.github.com>
2023-03-02 19:22:32 +0100

    Dodat model smeštaja

---
+++ frontend/src/models/smestaj.ts
@@ -0,0 +1,12 @@
+export class Smestaj{
+    id: number;
+    ime:  string;
+    tip:  string;
+    kategorija: number;
+    internet: boolean;
+    tv: boolean;
+    ac: boolean;
+    frizider: boolean;
+    sef: boolean;
+    fotografije: Array<string>;
+}
