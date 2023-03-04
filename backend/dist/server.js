c53a0bde76df99816a919d2c4bf088783be763cc
npotparic <85732213+npotparic@users.noreply.github.com>
2023-03-04 13:42:31 +0100

    Ispravljene greske

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -4,21 +4,39 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const mongoose_1 = __importDefault(require("mongoose"));
-const aranzman_routes_1 = __importDefault(require("./routes/aranzman.routes"));
-const korisnik_routes_1 = __importDefault(require("./routes/korisnik.routes"));
+const mongodb_1 = require("mongodb");
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
-app.use(express_1.default.json());
-mongoose_1.default.connect('mongodb://127.0.0.1:27017/turisticka_agencija'); // localhost zamenjen sa 127.0.0.1(resenje sa neta)
-const connection = mongoose_1.default.connection;
-connection.once('open', () => {
-    console.log('db connected');
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const dbName = 'turisticka_agencija';
+mongodb_1.MongoClient.connect(url, function (err, client) {
+    if (err) {
+        console.log('Error connecting to MongoDB', err);
+    }
+    else {
+        console.log('Connected to MongoDB successfully');
+        const db = client.db(dbName);
+        // Here you can define your Express routes that use the database connection
+        app.get('/', function (req, res) {
+            db.collection('myCollection').find().toArray(function (err, results) {
+                res.send(results);
+            });
+        });
+        // Start the Express app
+        app.listen(4000, function () {
+            console.log('Express app listening on port 4000');
+        });
+    }
 });
-const router = express_1.default.Router();
-router.use('/aranzman', aranzman_routes_1.default);
-router.use('/korisnici', korisnik_routes_1.default);
-app.use('/', router);
-app.listen(4000, () => console.log(`Express server running on port 4000`));
+// mongoose.connect('mongodb://database/turisticka_agencija') // localhost zamenjen sa 127.0.0.1(resenje sa neta)
+// const connection = mongoose.connection
+// connection.once('open', ()=>{
+//     console.log('db connected')
+// })
+// const router = express.Router();
+// var port = '4000';
+// app.set('port', port);
+// router.use('/aranzman', aranzmanRouter );
+// router.use('/korisnici', korisnikRouter );
+// app.use('/', router);
+// app.listen(400, () => console.log(`Express server running on njegov port `+ port));
 //# sourceMappingURL=server.js.map
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,49 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import aranzmanRouter from './routes/aranzman.routes';
-import korisnikRouter from './routes/korisnik.routes';
+import express, { Application, Request, Response } from 'express';
+import { MongoClient } from 'mongodb';
 
-const app = express();
-app.use(cors())
-app.use(express.json())
+const app: Application = express();
+
+const url: string = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const dbName: string = 'turisticka_agencija';
+
+MongoClient.connect(url, function(err, client) {
+  if (err) {
+    console.log('Error connecting to MongoDB', err);
+  } else {
+    console.log('Connected to MongoDB successfully');
+
+    const db = client.db(dbName);
+
+    // Here you can define your Express routes that use the database connection
+    app.get('/', function(req: Request, res: Response) {
+      db.collection('myCollection').find().toArray(function(err, results) {
+        res.send(results);
+      });
+    });
+
+    // Start the Express app
+    app.listen(4000, function() {
+      console.log('Express app listening on port 4000');
+    });
+  }
+});
+
+// mongoose.connect('mongodb://database/turisticka_agencija') // localhost zamenjen sa 127.0.0.1(resenje sa neta)
+
+// const connection = mongoose.connection
+// connection.once('open', ()=>{
+//     console.log('db connected')
+// })
+
+// const router = express.Router();
+
+// var port = '4000';
+
+// app.set('port', port);
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/turisticka_agencija') // localhost zamenjen sa 127.0.0.1(resenje sa neta)
+// router.use('/aranzman', aranzmanRouter );
+// router.use('/korisnici', korisnikRouter );
 
-const connection = mongoose.connection
-connection.once('open', ()=>{
-    console.log('db connected')
-})
-
-const router = express.Router();
-
-router.use('/aranzman', aranzmanRouter );
-router.use('/korisnici', korisnikRouter );
-
-app.use('/', router);
-app.listen(4000, () => console.log(`Express server running on port 4000`));
+// app.use('/', router);
+// app.listen(400, () => console.log(`Express server running on njegov port `+ port));
